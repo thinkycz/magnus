d53215dad839172b472ca1dd9370369fe74b6143
leo <leo@nulisec.com>
2020-02-10 23:51:23 +0100

    Article boxed

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <section class="bg-gray-100 py-24">
 
-        <div class="container max-w-5xl mx-auto">
+        <div class="container max-w-6xl mx-auto">
             <div class="py-24">
                 <h2 class="w-full text-4xl font-bold leading-tight text-center text-gray-700">{{ $article->title }}</h2>
                 <p class="text-center mt-2 text-sm text-gray-600">Ngày đăng bài viết {{ $article->created_at->format('j.n.Y') }}</p>
             </div>
 
-            <div class="editor">
+            <div class="editor bg-white rounded p-4 shadow-lg">
                 {!! $article->content !!}
             </div>
         </div>
