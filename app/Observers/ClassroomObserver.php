f4d0120d8068bf5af18d63f41ee3103ea3e06578
Long Do <me@thinky.cz>
2020-03-01 19:21:02 +0100

    Repeat course action implemented

--- app/Observers/ClassroomObserver.php
+++ app/Observers/ClassroomObserver.php
@@ -9,11 +9,13 @@ class ClassroomObserver
     public function created(Classroom $classroom)
     {
         for ($date = $classroom->starts_at; $date->lte($classroom->course->ends_at); $date->addDays($classroom->frequency_days)) {
-            $classroom->lessons()->create([
-                'starts_at' => $date,
-                'ends_at'   => $date->copy()->addMinutes($classroom->duration_minutes),
-                'course_id' => $classroom->course_id
-            ]);
+            if ($date->gte($classroom->course->starts_at)) {
+                $classroom->lessons()->create([
+                    'starts_at' => $date,
+                    'ends_at'   => $date->copy()->addMinutes($classroom->duration_minutes),
+                    'course_id' => $classroom->course_id
+                ]);
+            }
         }
     }
 }
