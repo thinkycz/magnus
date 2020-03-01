b21dbf3cd35f675221a292a433c55f1014c801c1
Long Do <me@thinky.cz>
2020-03-01 19:47:34 +0100

    Calendar changed first day to monday

--- nova-components/GlobalAgenda/resources/js/components/Card.vue
+++ nova-components/GlobalAgenda/resources/js/components/Card.vue
@@ -2,6 +2,7 @@
     <card class="flex flex-col items-center justify-center p-8" style="min-height: 300px">
         <FullCalendar
                 ref="fullCalendar"
+                :first-day="1"
                 :plugins="calendarPlugins"
                 :weekends="calendarWeekends"
                 :events="calendarEvents"
