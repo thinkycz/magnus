fdafbf3ef2caf06bf6ac9f4d9e04e749e7bd4252
Long Do <me@thinky.cz>
2020-03-14 09:21:36 +0100

    Copyright

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="https://mgns.cz" class="text-primary dim no-underline">Copyright of Magnus</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Made in Prague - By Anne Truong, Long Do, and the rest of Team Magnus. All Rights Reserved.
     <span class="px-1">&middot;</span>
     v{{ \Laravel\Nova\Nova::version() }}
 </p>
