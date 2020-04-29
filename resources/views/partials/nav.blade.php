a5f1c06e5e88d8918296bbd9758fcc9886fd954e
leo <leo@nulisec.com>
2020-04-29 21:25:13 +0200

    nav fix

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,40 +2,84 @@
 <div class="h-2 bg-green-600 w-full absolute z-20"></div>
 
 <!-- Nav Bar -->
-<div class="absolute w-full z-20">
-    <nav class="font-sans text-center flex justify-between my-4 mx-auto container overflow-hidden mt-8 z-50 relative px-5 overflow-visible">
-        <a href="/" class="toggleColour text-gray-700 no-underline hover:no-underline font-bold text-2xl lg:text-4xl h-10 pb-1 mx-auto sm:mx-0">
-            <svg class="h-full mt-1" viewBox="0 0 477 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><path d="M122.277,84.206l-39,-71.577l-15.553,29.456l-22.713,-38.585l-41.511,81.25" style="fill:#79cc9e;fill-opacity:0;stroke:url(#_Linear1);stroke-width:7px;"/><path d="M220.374,84.6l-8.404,-45.448l-15.455,46.414l-2.415,0l-15.89,-46.414l-8.211,45.448l-9.177,0l13.234,-70.756l4.395,0l16.856,51.63l15.552,-51.63l4.347,0l14.344,70.756l-9.176,0Z" style="fill:#2d3748;fill-rule:nonzero;"/><path d="M265.774,78.659c-3.671,4.605 -9.29,6.907 -16.856,6.907c-4.057,0 -7.575,-1.473 -10.553,-4.419c-2.979,-2.946 -4.468,-6.609 -4.468,-10.988c0,-5.248 2.294,-9.684 6.883,-13.306c4.588,-3.622 10.44,-5.434 17.556,-5.434c1.932,0 4.121,0.419 6.568,1.256c0,-8.372 -3.735,-12.557 -11.205,-12.557c-5.731,0 -10.142,1.545 -13.233,4.636l-3.864,-7.679c1.739,-1.417 4.145,-2.632 7.22,-3.647c3.075,-1.014 6.013,-1.521 8.815,-1.521c7.502,0 12.952,1.707 16.349,5.12c3.397,3.413 5.095,8.838 5.095,16.276l0,18.546c0,4.54 1.352,7.567 4.057,9.08l0,4.589c-3.735,0 -6.528,-0.532 -8.38,-1.594c-1.851,-1.063 -3.179,-2.817 -3.984,-5.265Zm-0.87,-19.464c-2.897,-0.644 -4.926,-0.966 -6.085,-0.966c-4.637,0 -8.42,1.192 -11.35,3.574c-2.93,2.383 -4.395,5.201 -4.395,8.453c0,5.377 3.171,8.065 9.514,8.065c4.637,0 8.742,-2.205 12.316,-6.616l0,-12.51Z" style="fill:#2d3748;fill-rule:nonzero;"/><path d="M286.638,98.751l4.975,-7.341c5.345,3.574 10.271,5.361 14.779,5.361c4.154,0 7.43,-0.716 9.829,-2.149c2.399,-1.433 3.598,-3.212 3.598,-5.337c0,-4.186 -3.027,-6.279 -9.08,-6.279c-1.03,0 -2.898,0.258 -5.602,0.773c-2.705,0.515 -4.814,0.773 -6.327,0.773c-7.342,0 -11.012,-2.769 -11.012,-8.307c0,-1.707 0.861,-3.253 2.584,-4.637c1.722,-1.385 3.855,-2.383 6.399,-2.995c-7.277,-3.413 -10.915,-9.337 -10.915,-17.773c0,-5.41 1.883,-9.917 5.651,-13.524c3.767,-3.606 8.419,-5.409 13.958,-5.409c5.087,0 9.064,1.046 11.929,3.139l4.589,-5.506l5.988,5.651l-5.505,4.154c2.35,3.059 3.525,7.051 3.525,11.978c0,5.216 -1.626,9.595 -4.878,13.137c-3.252,3.542 -7.518,5.57 -12.799,6.085l-7.583,0.773c-0.901,0.097 -2.109,0.443 -3.622,1.039c-1.513,0.595 -2.27,1.376 -2.27,2.342c0,1.32 1.578,1.98 4.733,1.98c1.385,0 3.542,-0.249 6.472,-0.748c2.93,-0.499 5.104,-0.749 6.52,-0.749c5.088,0 9.056,1.215 11.906,3.646c2.849,2.431 4.274,5.804 4.274,10.119c0,4.765 -2.133,8.613 -6.399,11.543c-4.267,2.93 -9.668,4.395 -16.204,4.395c-3.349,0 -6.875,-0.595 -10.577,-1.787c-3.703,-1.191 -6.682,-2.64 -8.936,-4.347Zm19.465,-59.454c-3.188,0 -5.788,1.127 -7.801,3.38c-2.012,2.254 -3.018,4.991 -3.018,8.211c0,3.606 0.974,6.593 2.922,8.959c1.948,2.367 4.58,3.55 7.897,3.55c3.252,0 5.795,-1.151 7.631,-3.453c1.835,-2.302 2.753,-5.321 2.753,-9.056c0,-3.22 -0.991,-5.957 -2.971,-8.211c-1.98,-2.253 -4.451,-3.38 -7.413,-3.38Z" style="fill:#2d3748;fill-rule:nonzero;"/><path d="M370.001,84.6l0,-30.09c0,-5.506 -0.829,-9.361 -2.488,-11.567c-1.658,-2.206 -4.435,-3.308 -8.331,-3.308c-2.093,0 -4.282,0.628 -6.569,1.883c-2.286,1.256 -4.041,2.802 -5.264,4.637l0,38.445l-9.177,0l0,-51.727l6.279,0l2.898,6.665c3.027,-5.087 7.969,-7.631 14.827,-7.631c11.334,0 17.001,6.891 17.001,20.672l0,32.021l-9.176,0Z" style="fill:#2d3748;fill-rule:nonzero;"/><path d="M400.911,32.873l0,32.987c0,7.986 3.462,11.978 10.384,11.978c3.027,0 5.796,-0.869 8.308,-2.608c2.511,-1.738 4.169,-3.751 4.974,-6.037l0,-36.32l9.177,0l0,51.727l-9.177,0l0,-7.148c-1.03,2.028 -3.099,3.888 -6.206,5.578c-3.107,1.691 -6.142,2.536 -9.104,2.536c-5.667,0 -10.006,-1.626 -13.016,-4.878c-3.011,-3.252 -4.516,-7.873 -4.516,-13.862l0,-33.953l9.176,0Z" style="fill:#2d3748;fill-rule:nonzero;"/><path d="M442.882,81.509l3.236,-8.694c5.12,3.349 9.241,5.023 12.364,5.023c5.667,0 8.501,-2.382 8.501,-7.148c0,-3.413 -2.737,-6.343 -8.211,-8.79c-4.218,-1.932 -7.059,-3.397 -8.524,-4.395c-1.465,-0.998 -2.737,-2.133 -3.816,-3.405c-1.079,-1.272 -1.883,-2.624 -2.415,-4.057c-0.531,-1.433 -0.797,-2.97 -0.797,-4.613c0,-4.25 1.546,-7.566 4.637,-9.949c3.091,-2.383 7.132,-3.574 12.123,-3.574c3.767,0 8.516,1.191 14.248,3.574l-2.609,8.501c-3.638,-2.898 -7.293,-4.347 -10.963,-4.347c-2.19,0 -4.033,0.515 -5.53,1.545c-1.497,1.031 -2.246,2.335 -2.246,3.912c0,3.317 1.884,5.828 5.651,7.535l6.568,2.994c4.025,1.836 6.955,3.929 8.79,6.279c1.836,2.351 2.753,5.297 2.753,8.839c0,4.636 -1.626,8.267 -4.878,10.891c-3.252,2.624 -7.759,3.936 -13.523,3.936c-5.442,0 -10.561,-1.352 -15.359,-4.057Z" style="fill:#2d3748;fill-rule:nonzero;"/><defs><linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-118.777,0,0,81.2503,122.277,44.1251)"><stop offset="0" style="stop-color:#48bb78;stop-opacity:1"/><stop offset="1" style="stop-color:#38a169;stop-opacity:1"/></linearGradient></defs></svg>
-        </a>
-        <ul class="text-sm text-grey-dark list-reset flex items-center">
-            <li class="mr-8 group relative hidden sm:block">
+<div class="w-full z-20 pt-4">
+    <div class="mx-auto container">
+        <nav class="font-sans text-center md:flex justify-between py-4 overflow-hidden z-50 relative px-5 overflow-visible">
+            <a href="/"
+               class="toggleColour text-gray-700 no-underline hover:no-underline font-bold text-2xl lg:text-4xl h-10 pb-1 flex justify-center">
+                <svg class="h-12" viewBox="0 0 477 105" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                     xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"
+                     style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><path
+                        d="M122.277,84.206l-39,-71.577l-15.553,29.456l-22.713,-38.585l-41.511,81.25"
+                        style="fill:#79cc9e;fill-opacity:0;stroke:url(#_Linear1);stroke-width:7px;"/>
+                    <path
+                        d="M220.374,84.6l-8.404,-45.448l-15.455,46.414l-2.415,0l-15.89,-46.414l-8.211,45.448l-9.177,0l13.234,-70.756l4.395,0l16.856,51.63l15.552,-51.63l4.347,0l14.344,70.756l-9.176,0Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <path
+                        d="M265.774,78.659c-3.671,4.605 -9.29,6.907 -16.856,6.907c-4.057,0 -7.575,-1.473 -10.553,-4.419c-2.979,-2.946 -4.468,-6.609 -4.468,-10.988c0,-5.248 2.294,-9.684 6.883,-13.306c4.588,-3.622 10.44,-5.434 17.556,-5.434c1.932,0 4.121,0.419 6.568,1.256c0,-8.372 -3.735,-12.557 -11.205,-12.557c-5.731,0 -10.142,1.545 -13.233,4.636l-3.864,-7.679c1.739,-1.417 4.145,-2.632 7.22,-3.647c3.075,-1.014 6.013,-1.521 8.815,-1.521c7.502,0 12.952,1.707 16.349,5.12c3.397,3.413 5.095,8.838 5.095,16.276l0,18.546c0,4.54 1.352,7.567 4.057,9.08l0,4.589c-3.735,0 -6.528,-0.532 -8.38,-1.594c-1.851,-1.063 -3.179,-2.817 -3.984,-5.265Zm-0.87,-19.464c-2.897,-0.644 -4.926,-0.966 -6.085,-0.966c-4.637,0 -8.42,1.192 -11.35,3.574c-2.93,2.383 -4.395,5.201 -4.395,8.453c0,5.377 3.171,8.065 9.514,8.065c4.637,0 8.742,-2.205 12.316,-6.616l0,-12.51Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <path
+                        d="M286.638,98.751l4.975,-7.341c5.345,3.574 10.271,5.361 14.779,5.361c4.154,0 7.43,-0.716 9.829,-2.149c2.399,-1.433 3.598,-3.212 3.598,-5.337c0,-4.186 -3.027,-6.279 -9.08,-6.279c-1.03,0 -2.898,0.258 -5.602,0.773c-2.705,0.515 -4.814,0.773 -6.327,0.773c-7.342,0 -11.012,-2.769 -11.012,-8.307c0,-1.707 0.861,-3.253 2.584,-4.637c1.722,-1.385 3.855,-2.383 6.399,-2.995c-7.277,-3.413 -10.915,-9.337 -10.915,-17.773c0,-5.41 1.883,-9.917 5.651,-13.524c3.767,-3.606 8.419,-5.409 13.958,-5.409c5.087,0 9.064,1.046 11.929,3.139l4.589,-5.506l5.988,5.651l-5.505,4.154c2.35,3.059 3.525,7.051 3.525,11.978c0,5.216 -1.626,9.595 -4.878,13.137c-3.252,3.542 -7.518,5.57 -12.799,6.085l-7.583,0.773c-0.901,0.097 -2.109,0.443 -3.622,1.039c-1.513,0.595 -2.27,1.376 -2.27,2.342c0,1.32 1.578,1.98 4.733,1.98c1.385,0 3.542,-0.249 6.472,-0.748c2.93,-0.499 5.104,-0.749 6.52,-0.749c5.088,0 9.056,1.215 11.906,3.646c2.849,2.431 4.274,5.804 4.274,10.119c0,4.765 -2.133,8.613 -6.399,11.543c-4.267,2.93 -9.668,4.395 -16.204,4.395c-3.349,0 -6.875,-0.595 -10.577,-1.787c-3.703,-1.191 -6.682,-2.64 -8.936,-4.347Zm19.465,-59.454c-3.188,0 -5.788,1.127 -7.801,3.38c-2.012,2.254 -3.018,4.991 -3.018,8.211c0,3.606 0.974,6.593 2.922,8.959c1.948,2.367 4.58,3.55 7.897,3.55c3.252,0 5.795,-1.151 7.631,-3.453c1.835,-2.302 2.753,-5.321 2.753,-9.056c0,-3.22 -0.991,-5.957 -2.971,-8.211c-1.98,-2.253 -4.451,-3.38 -7.413,-3.38Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <path
+                        d="M370.001,84.6l0,-30.09c0,-5.506 -0.829,-9.361 -2.488,-11.567c-1.658,-2.206 -4.435,-3.308 -8.331,-3.308c-2.093,0 -4.282,0.628 -6.569,1.883c-2.286,1.256 -4.041,2.802 -5.264,4.637l0,38.445l-9.177,0l0,-51.727l6.279,0l2.898,6.665c3.027,-5.087 7.969,-7.631 14.827,-7.631c11.334,0 17.001,6.891 17.001,20.672l0,32.021l-9.176,0Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <path
+                        d="M400.911,32.873l0,32.987c0,7.986 3.462,11.978 10.384,11.978c3.027,0 5.796,-0.869 8.308,-2.608c2.511,-1.738 4.169,-3.751 4.974,-6.037l0,-36.32l9.177,0l0,51.727l-9.177,0l0,-7.148c-1.03,2.028 -3.099,3.888 -6.206,5.578c-3.107,1.691 -6.142,2.536 -9.104,2.536c-5.667,0 -10.006,-1.626 -13.016,-4.878c-3.011,-3.252 -4.516,-7.873 -4.516,-13.862l0,-33.953l9.176,0Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <path
+                        d="M442.882,81.509l3.236,-8.694c5.12,3.349 9.241,5.023 12.364,5.023c5.667,0 8.501,-2.382 8.501,-7.148c0,-3.413 -2.737,-6.343 -8.211,-8.79c-4.218,-1.932 -7.059,-3.397 -8.524,-4.395c-1.465,-0.998 -2.737,-2.133 -3.816,-3.405c-1.079,-1.272 -1.883,-2.624 -2.415,-4.057c-0.531,-1.433 -0.797,-2.97 -0.797,-4.613c0,-4.25 1.546,-7.566 4.637,-9.949c3.091,-2.383 7.132,-3.574 12.123,-3.574c3.767,0 8.516,1.191 14.248,3.574l-2.609,8.501c-3.638,-2.898 -7.293,-4.347 -10.963,-4.347c-2.19,0 -4.033,0.515 -5.53,1.545c-1.497,1.031 -2.246,2.335 -2.246,3.912c0,3.317 1.884,5.828 5.651,7.535l6.568,2.994c4.025,1.836 6.955,3.929 8.79,6.279c1.836,2.351 2.753,5.297 2.753,8.839c0,4.636 -1.626,8.267 -4.878,10.891c-3.252,2.624 -7.759,3.936 -13.523,3.936c-5.442,0 -10.561,-1.352 -15.359,-4.057Z"
+                        style="fill:#2d3748;fill-rule:nonzero;"/>
+                    <defs>
+                        <linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse"
+                                        gradientTransform="matrix(-118.777,0,0,81.2503,122.277,44.1251)">
+                            <stop offset="0" style="stop-color:#48bb78;stop-opacity:1"/>
+                            <stop offset="1" style="stop-color:#38a169;stop-opacity:1"/>
+                        </linearGradient>
+                    </defs></svg>
+            </a>
+            <ul class="text-sm text-grey-dark list-reset flex flex-col md:flex-row items-center mt-6 md:mt-0">
+                <li class="mt-4 md:mt-0 md:mr-8 group relative">
                 <span class="font-semibold text-gray-900 hover:text-orange-500 cursor-pointer">
                     Tiếng Séc Giao Tiếp
                 </span>
 
-                <div class="absolute hidden group-hover:flex flex-col bg-white shadow rounded top-0 right-0 mt-4 py-2">
-                    <a href="{{ route('category', 'luyen-nghe-tieng-sec') }}" class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Luyện nghe tiếng Séc</a>
-                    <a href="{{ route('category', 'luyen-noi-tieng-sec') }}" class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Luyện nói tiếng Séc</a>
-                    <a href="{{ route('category', 'phat-am-tieng-sec') }}" class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Phát âm tiếng Séc</a>
-                    <a href="{{ route('category', 'ngu-phap-tieng-sec') }}" class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Ngữ pháp tiếng Séc</a>
-                    <a href="{{ route('category', 'tai-lieu-cua-tt-magnus') }}" class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Tài liệu của TT Magnus</a>
-                </div>
-            </li>
-            <li class="mr-8 hidden hidden md:block">
-                <a href="{{ route('courses') }}" class="font-semibold text-gray-900 hover:text-orange-500">Các khoá học</a>
-            </li>
-            <li class="mr-8 hidden lg:block">
-                <a href="{{ route('category', 've-tt-magnus') }}" class="font-semibold text-gray-900 hover:text-orange-500">Về TT Magnus</a>
-            </li>
-            @auth
-                <li class="hidden sm:block">
-                    <a href="{{ route('dashboard') }}" class="text-white ml-4 py-3 px-6 rounded-full bg-gray-800 font-bold bg-green-500 hover:bg-green-600">Vào ứng dụng</a>
+                    <div
+                        class="absolute hidden group-hover:flex flex-col bg-white shadow rounded top-0 right-0 mt-4 py-2">
+                        <a href="{{ route('category', 'luyen-nghe-tieng-sec') }}"
+                           class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Luyện nghe tiếng Séc</a>
+                        <a href="{{ route('category', 'luyen-noi-tieng-sec') }}"
+                           class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Luyện nói tiếng Séc</a>
+                        <a href="{{ route('category', 'phat-am-tieng-sec') }}"
+                           class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Phát âm tiếng Séc</a>
+                        <a href="{{ route('category', 'ngu-phap-tieng-sec') }}"
+                           class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Ngữ pháp tiếng Séc</a>
+                        <a href="{{ route('category', 'tai-lieu-cua-tt-magnus') }}"
+                           class="px-6 py-2 hover:bg-gray-200 whitespace-no-wrap">Tài liệu của TT Magnus</a>
+                    </div>
                 </li>
-            @else
-                <li class="hidden sm:block">
-                    <a href="{{ route('login') }}" class="text-white ml-4 py-3 px-6 rounded-full bg-gray-800 font-bold bg-green-500 hover:bg-green-600">Đăng nhập</a>
+                <li class="mt-4 md:mt-0 md:mr-8">
+                    <a href="{{ route('courses') }}" class="font-semibold text-gray-900 hover:text-orange-500">Các khoá
+                        học</a>
                 </li>
-            @endauth
-        </ul>
-    </nav>
+                <li class="mt-4 md:mt-0 md:mr-8">
+                    <a href="{{ route('category', 've-tt-magnus') }}"
+                       class="font-semibold text-gray-900 hover:text-orange-500">Về TT Magnus</a>
+                </li>
+                @auth
+                    <li class="hidden lg:block">
+                        <a href="{{ route('dashboard') }}"
+                           class="text-white ml-4 py-3 px-6 rounded-full bg-gray-800 font-bold bg-green-500 hover:bg-green-600">Vào
+                            ứng dụng</a>
+                    </li>
+                @else
+                    <li class="hidden lg:block">
+                        <a href="{{ route('login') }}"
+                           class="text-white ml-4 py-3 px-6 rounded-full bg-gray-800 font-bold bg-green-500 hover:bg-green-600">Đăng
+                            nhập</a>
+                    </li>
+                @endauth
+            </ul>
+        </nav>
+    </div>
 </div>
